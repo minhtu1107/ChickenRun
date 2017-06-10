860b1576c0f8b1551bc88b0a7bae430497457499
minhtu1107 <minhtu1107@yahoo.com>
2017-06-10 14:55:04 +0700

    - update

--- batgamoi/desktop/src/com/catchchicken/game/desktop/DesktopLauncher.java
+++ batgamoi/desktop/src/com/catchchicken/game/desktop/DesktopLauncher.java
@@ -5,10 +5,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.catchchicken.game.CatchChickenGame;
 
 public class DesktopLauncher {
+	static CatchChickenGame mygame;
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 960;
 		config.height = 540;
-		new LwjglApplication(new CatchChickenGame(new ActionResolverDesktop()), config);
+		mygame = new CatchChickenGame();
+		mygame.setActionResolver(new ActionResolverDesktop());
+		new LwjglApplication(mygame, config);
 	}
 }
